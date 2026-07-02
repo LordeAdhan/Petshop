e023ee35cdfa05f1d1a12f13d5f6f2b8197c27af
AdhanTheFullStack <adhan.benites@estudante.ifms.edu.br>
2026-07-02 10:05:37 -0400

    Add initial App.vue with navigation links

---
+++ App.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+  <h1> Petshop Info <div style="flex-grow: 1"></div>
+    <router-link to="/">Principal</router-link>
+    <router-link to="/cadastroCliente">Cadastro Cliente</router-link>
+    <router-link to="/cadastroPet">Cadastro Pet</router-link>
+    <router-link to="/cadastroServico">Cadastro Serviço</router-link>
+  </h1>
+  <router-view></router-view>
+</template>
+
+<style scoped>
+  h1{
+    display: flex;
+    color: darkgreen;
+    width: 100%;
+    margin: 0;
+    background-color: lightgray;
+    padding-top: 0.5em;
+    padding-bottom: 0.5em;
+  }
+  a{
+    background-color: darkgreen;
+    color: white;
+    padding: 0.5em;
+    margin-right: 1em;
+    text-decoration: none;
+    border-bottom: 5px solid darkgreen;
+    font-size: 0.5em;
+  }
+  a:hover{
+    border-bottom: 5px solid lightgreen;
+  }
+</style>
